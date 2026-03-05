dad0a3284899227915f28baa252a80385ccb3d4d
Jeroen van Duijn <jeroen@crossfitleiden.com>
2026-03-05 14:14:31 +0100

    feat: install Meta Pixel (1285596611453953) on all pages

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -116,6 +116,26 @@ export default async function LocaleLayout({
             gtag('config', 'G-JPD4QBBYZ3');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1285596611453953');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1285596611453953&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Analytics />
         <SpeedInsights />
       </body>
